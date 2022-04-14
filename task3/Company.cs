b42a211cadf18fa92f1bbbdde82ac5929343abea
ULiesnik <90095076+ULiesnik@users.noreply.github.com>
2022-04-14 03:00:36 +0300

    task3

---
+++ task3/Company.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pract_sem4_t3
+{
+    class Company
+    {
+        private string users;
+        private string drafts;
+        private string currentUser;
+
+        public Company() { }
+        public Company(string users_file, string records_file)
+        {
+            Users = Confirm.file_name_read(users_file);
+            Records = Confirm.file_name_read(records_file);
+            CurrentUser = null;
+        }
+
+        public void SignUp()
+        {
+            Console.WriteLine("\nEnter your data in format below (white spaces between elements):");
+            Console.WriteLine("FirstName LastName Email Password Salary FirstDayInCompany");
+            Staff you = new Staff(Console.ReadLine());
+            using (StreamWriter sw = File.AppendText(this.Users))
+            {
+                sw.WriteLine(you.ToString());
+            }
+            CurrentUser = you.ToString();
+        }
+
+        public void SignIn()
+        {
+            User[] u = Confirm.users_read(this.Users);
+            bool signed = false;
+            while (!signed)
+            {
+                Console.WriteLine("\nEnter your data in format below to sign in(white spaces between elements):");
+                Console.WriteLine("Email Password");
+                string data = Console.ReadLine();
+                for (int i = 0; i < u.Length; i++)
+                {
+                    if (u[i].IsMe(data))
+                    {
+                        CurrentUser = u[i].ToString();
+                        signed = true;
+                        break;
+                    }
+                }
+            }
+        }
+        public void logout()
+        {
+            CurrentUser = null;
+        }
+
+        public string Users
+        {
+            get { return users; }
+            set { users = value; }
+        }
+
+        public string Records
+        {
+            get { return drafts; }
+            set { drafts = value; }
+        }
+
+        public string CurrentUser
+        {
+            get { return currentUser; }
+            set { currentUser = value; }
+        }
+    }
+}
---
+++ task3/Record.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pract_sem4_t3
+{
+    class Record
+    {
+        private Transaction item;
+        private string userName;
+        private string status;
+        private string message;
+
+        public Record() { }
+
+        public Record(string transaction_data, string name, string stat, string mess = "No message here")
+        {
+            Item = new Transaction(transaction_data);
+            UserName = name;
+            Status = stat;
+            Message = mess;
+        }
+
+        public void Approve( string mess = "No message here")
+        {
+            Status = "Approved";
+            Message = mess;
+        }
+
+        public void Reject(string mess = "No message here")
+        {
+            Status = "Rejected";
+            Message = mess;
+
+        }
+
+        public void Decide()
+        {
+            Console.WriteLine("Choose whether you want to approve, reject or pass this draft");
+            string[] ops = { "approve", "reject", "pass" };
+            switch (Confirm.choise_input(ops))
+            {
+                case "approve":
+                    Console.WriteLine("Message:");
+                    this.Approve(Console.ReadLine());
+                    break;
+                case "reject":
+                    Console.WriteLine("Message:");
+                    this.Reject(Console.ReadLine());
+                    break;
+                case "pass":
+                    break;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{Item.ToString()}\n{UserName}\n{Status}\n{Message}";
+        }
+
+        public void ShowInfo()
+        {
+            Item.ShowInfo();
+            Console.WriteLine($"Transaction added by {UserName} - status is {Status}\n{Message}\n");
+            Console.WriteLine("------------------------------------------------------------------------\n");
+        }
+
+        public Transaction Item
+        {
+            get { return item; }
+            set { item = value; }
+        }
+
+        public string UserName
+        {
+            get { return userName; }
+            set
+            {
+                while (!Confirm.full_name_condition(value))
+                {
+                    Console.WriteLine("\nUser name data is invalid! Please enter new value!");
+                    Console.Write("New name: ");
+                    value = Console.ReadLine();
+                }
+                userName = value;
+            }
+        }
+
+        public string Status
+        {
+            get { return status; }
+            set
+            {
+                while (!Confirm.status_condition(value))
+                {
+                    Console.WriteLine("\nStatus data is invalid! Please enter new value!");
+                    Console.Write("New status: ");
+                    value = Console.ReadLine();
+                }
+                status = value;
+            }
+        }
+
+        public string Message
+        {
+            get { return message; }
+            set { message = value; }
+        }
+    }
+}
