0cd13648025a6c74a064569be59409e997e88e13
ULiesnik <90095076+ULiesnik@users.noreply.github.com>
2022-04-28 02:40:58 +0300

    Task4

---
+++ task4/Models/Transaction.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PracticeAPISem4.Models
+{
+    public class Transaction
+    {
+        public string Name { get; set; }
+        public string Id { get; set; }
+        public string CardNumber { get; set; }
+        public string Cvc { get; set; }
+        public int Month { get; set; }
+        public int Year { get; set; }
+        public DateTime Date { get; set; }
+        public int Amount { get; set; }
+    }
+}
